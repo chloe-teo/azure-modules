37b21c17bcb1dcb259b845b4316316970f7b8613
chloe-teo <teotab08@gmail.com>
2026-07-28 19:10:19 +0200

    updated aks module

--- azure-kubernetes-service/outputs.tf
+++ azure-kubernetes-service/outputs.tf
@@ -5,8 +5,3 @@ output "id" {
 output "name" {
   value = azurerm_kubernetes_cluster.this.name
 }
-
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.this.kube_config_raw
-  sensitive = true
-}

449c1cfeb5e005d54d9ffbd249a61a3782b57504
chloe-teo <teotab08@gmail.com>
2026-07-28 18:59:16 +0200

    updated aks module

--- azure-kubernetes-service/main.tf
+++ azure-kubernetes-service/main.tf
@@ -33,10 +33,10 @@ resource "azurerm_kubernetes_cluster" "this" {
     dns_service_ip    = var.dns_service_ip
   }
 
-  azure_active_directory_role_based_access_control{
-    azure_rbac_enabled = true
-    admin_group_object_ids = var.admin_group_object_ids
-  }
+  # azure_active_directory_role_based_access_control{
+  #   azure_rbac_enabled = true
+  #   admin_group_object_ids = var.admin_group_object_ids
+  # }
 
   tags = var.tags
 

504c902dc2e5dc066c90f9e6eee4bd97eccf40a8
chloe-teo <teotab08@gmail.com>
2026-08-03 14:10:33 +0200

    updated code

--- azure-application-insight/outputs.tf
+++ azure-application-insight/outputs.tf
@@ -1,7 +1,7 @@
 output "instrumentation_key" {
-  value = azurerm_application_insights.example.instrumentation_key
+  value = azurerm_application_insights.this.instrumentation_key
 }
 
 output "app_id" {
-  value = azurerm_application_insights.example.app_id
+  value = azurerm_application_insights.this.app_id
 }
